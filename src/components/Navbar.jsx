5918097fcd3ac5359dd9d73952e1d5b0dab9bc0b
creatijourny <misbahulhaque.gmail.com>
2026-06-05 22:51:57 +0600

    project setup, heroUI installed, navbar and banner added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+'use client'
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className='flex justify-between p-5'>
+      <div>
+        <h2 className='text-3xl font-bold'>Study<span className='text-teal-500'>Nest</span></h2>
+      </div>
+      <ul className='flex gap-3'>
+        <li><Link href={'/'}>Home</Link></li>
+        <li><Link href={'/rooms'}>Rooms</Link></li>
+        <li><Link href={'/add-room'}>Add Room</Link></li>
+        <li><Link href={'/my-bookings'}>My Bookings</Link></li>
+      </ul>
+      {/* <div>
+        <Image 
+        src={'/assets/mypic.jpg'}
+        height={150}
+        width={150}
+        alt='logo'/>
+      </div> */}
+      <ul className='flex gap-3'>
+        <li><Link href={'/profile'}>Profile</Link></li>
+        <li><Link href={'/login'}>Login</Link></li>
+        <li><Link href={'/signup'}>Sign Up</Link></li>        
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
